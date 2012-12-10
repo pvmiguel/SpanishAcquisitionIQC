8b2b1a696e31f0bcab46951ecb2ff05f037b19d9
Grant <ghwatson@server.fake>
2012-12-10 17:10:12 -0500

    Added the agilent dm34401 device.

--- spacq/devices/agilent/__init__.py
+++ spacq/devices/agilent/__init__.py
@@ -4,10 +4,10 @@ log = logging.getLogger(__name__)
 
 name = 'Agilent'
 
-from . import dm34410a
-models = [dm34410a]
+from . import dm34410a, dm34401a
+models = [dm34410a, dm34401a]
 log.debug('Found models for "{0}": {1}'.format(name, ''.join(str(x) for x in models)))
 
-from .mock import mock_dm34410a
-mock_models = [mock_dm34410a]
+from .mock import mock_dm34410a, mock_dm34401a
+mock_models = [mock_dm34410a, mock_dm34401a]
 log.debug('Found mock models for "{0}": {1}'.format(name, ''.join(str(x) for x in mock_models)))
