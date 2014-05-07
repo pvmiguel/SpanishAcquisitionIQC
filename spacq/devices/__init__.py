fda823bd3a8f9849ab31854301c489b28ea1bcea
kylewillick <kylewillick@gmail.com>
2014-05-07 10:34:43 -0400

    Fixed device list
    devices\__init__.py was missing stanford_research_systems to include the
    network analyzer

--- spacq/devices/__init__.py
+++ spacq/devices/__init__.py
@@ -1,2 +1,2 @@
-from . import agilent, iqc, oxford, rohde_schwarz, tektronix, keithley, cryomagnetics, lakeshore
-manufacturers = [agilent, iqc, oxford, rohde_schwarz, tektronix, keithley, cryomagnetics, lakeshore]
+from . import agilent, iqc, oxford, rohde_schwarz, tektronix, keithley, cryomagnetics, lakeshore, stanford_research_systems
+manufacturers = [agilent, iqc, oxford, rohde_schwarz, tektronix, keithley, cryomagnetics, lakeshore, stanford_research_systems]
